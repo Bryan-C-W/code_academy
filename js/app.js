8d8c35c116bf07ab6ce83384fcc7e312e5e39332
Bryan-C-W <jabrway96@gmail.com>
2017-12-05 19:20:09 -0600

    newborder

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $("#hobbytitle").click(function(){
         $("#hobbiesp").toggle();
     })
+    $("#skillstitle").click(function(){
+        $("#skillsp").toggle();
+    })
 })
