ee05a78ee6236357621d95bf75e22532b52b5e18
Bryan-C-W <jabrway96@gmail.com>
2017-12-12 19:40:21 -0600

    file structer

--- js/app.js
+++ js/app.js
@@ -2,16 +2,26 @@ $(document).ready(function () {
     $("#btnmain button").on("click", function(){
         $("#textmain").toggle();
         $("#skills").toggle();
+        $("#textskill").hide();
+        $("#btnskill button").show();
         $("#hobbies").toggle();
+        $("#texthobbies").hide();
+        $("#btnhobbies button").show();
         $("#contact").toggle();
+        $("#textcontact").hide();
+        $("#btncontact button").show();
+        
     })
-    $("#btnskill button").click(function(){
+    $("#btnskill button").on("click",function(){
         $("#textskill").toggle();
+        $("#btnskill button").toggle();
     })
-    $("#btnhobbies button").click(function(){
+    $("#btnhobbies button").on("click",function(){
         $("#texthobbies").toggle();
+        $("#btnhobbies button").toggle();
     })
-    $("#btncontact button").click(function(){
+    $("#btncontact button").on("click",function(){
         $("#textcontact").toggle();
-    })
+        $("#btncontact button").toggle()
+})
 })
